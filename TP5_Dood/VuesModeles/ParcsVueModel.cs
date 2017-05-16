d36c12e381f88f28962eb3dacbedf9bfe7798806
olibombardier <olibombardier@gmail.com>
2017-05-16 14:10:39 -0400

    Bd rétrogradée à une version plus basse

--- TP5_Dood/VuesModeles/ParcsVueModel.cs
+++ TP5_Dood/VuesModeles/ParcsVueModel.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TP5_Dood.VuesModeles;
+using TP5_Dood.Modeles;
 
 namespace TP5_Dood.VuesModeles
 {
-    class ParcsVueModel : VueModeleBase
+    class ParcsVueModel : VueModeleBase, IDisposable
     {
+        private BdEntities context;
 
+        public ParcsVueModel()
+        {
+            context = new BdEntities();
+        }
+
+        ~ParcsVueModel()
+        {
+            Dispose();
+        }
+
+        public List<Parc> Parcs
+        {
+            get
+            { 
+                return context.Parcs.ToList();
+            }
+        }
+
+        public void Dispose()
+        {
+            context?.Dispose();
+        }
     }
 }
